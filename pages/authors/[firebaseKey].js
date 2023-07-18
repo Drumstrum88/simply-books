f451f9c9d596a1ddd9cabd5fe05e6d3a3a70724d
drumstrum88 <rev.jsanderson@gmail.com>
2023-07-18 18:01:34 -0500

    viewing and delete author

--- pages/authors/[firebaseKey].js
+++ pages/authors/[firebaseKey].js
@@ -1,15 +1,47 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import BookCard from '../../components/BookCard';
+import { viewAuthorDetails } from '../../api/mergedData';
 
 export default function AuthorDetails() {
+  const [authorData, setAuthorData] = useState(null);
   const router = useRouter();
+
   const { firebaseKey } = router.query;
 
+  useEffect(() => {
+    if (firebaseKey) {
+      viewAuthorDetails(firebaseKey)
+        .then((data) => setAuthorData(data))
+        .catch((error) => {
+          console.error('Error fetching author details:', error);
+          setAuthorData(null);
+        });
+    }
+  }, [firebaseKey]);
+
+  if (!authorData) {
+    return <p>Loading author details...</p>;
+  }
+
+  const {
+    first_name, last_name, email, books,
+  } = authorData;
+
   return (
-    <div className="mt-5 d-flex flex-wrap">
-      <div className="d-flex flex-column">
-        <p>author test ${ firebaseKey }</p>
-      </div>
+    <div>
+      <h1>Author Details</h1>
+      <h2>{first_name} {last_name}</h2>
+      <p>{email}</p>
+      <h3>Books by {first_name}:</h3>
+      {Array.isArray(books) && books.length > 0 ? (
+        books.map((book) => (
+          <BookCard key={book.firebaseKey} bookObj={book} />
+        ))
+      ) : (
+        <p>No books found for this author.</p>
+      )}
     </div>
   );
 }
--- pages/authors/authors.js
+++ pages/authors/authors.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 import { useAuth } from '../../utils/context/authContext';
-import { getAuthors } from '../../api/authorData';
+import { deleteSingleAuthor, getAuthors } from '../../api/authorData';
 import AuthorCard from '../../components/authorCard';
 
 function Authors() {
@@ -17,6 +17,16 @@ function Authors() {
     getAllTheAuthors();
   }, [getAllTheAuthors]);
 
+  const onDeleteAuthor = (authorId) => {
+    deleteSingleAuthor(authorId)
+      .then(() => {
+        getAllTheAuthors();
+      })
+      .catch((error) => {
+        console.error('Error deleting author:', error);
+      });
+  };
+
   return (
     <div className="text-center my-4">
       <Link passHref href="/authors/new">
@@ -24,7 +34,12 @@ function Authors() {
       </Link>
       <div className="d-flex flex-wrap">
         {authors.map((author) => (
-          <AuthorCard key={author.firebaseKey} authorObj={author} onUpdate={getAllTheAuthors} />
+          <AuthorCard
+            key={author.firebaseKey}
+            authorObj={author}
+            onUpdate={getAllTheAuthors}
+            onDeleteAuthor={onDeleteAuthor}
+          />
         ))}
       </div>
     </div>
